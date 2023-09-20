1fc137f65d62179cc5756d6e734d07ffafcd5b80
L1verly <artemartem7975@gmail.com>
2023-09-20 18:32:21 +0300

    3: Deployment

--- components/index.js
+++ components/index.js
@@ -14,6 +14,7 @@ import Specifics from "./jobdetails/specifics/Specifics";
 
 // common
 import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
+import PopularJobCard from "./common/cards/popular/PopularJobCard";
 
 export {
   ScreenHeaderBtn,
@@ -25,5 +26,6 @@ export {
   JobAbout,
   JobFooter,
   Specifics,
-  NearbyJobCard
+  NearbyJobCard,
+  PopularJobCard
 };
--- constants/theme.js
+++ constants/theme.js
@@ -44,7 +44,7 @@ const SHADOWS = {
     },
     shadowOpacity: 0.25,
     shadowRadius: 5.84,
-    elevation: 5,
+    elevation: 3,
   },
 };
 
